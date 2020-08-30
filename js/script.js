e0a8228cda6bbb8649e9d88a4fab574dfd3f26c8
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2020-08-30 22:17:10 +0200

    adds script.js for slider and fixed navbar

---
+++ js/script.js
@@ -0,0 +1,50 @@
+const nextBtn = document.querySelector(".arrow-right");
+const prevBtn = document.querySelector(".arrow-left");
+const bgContainer = document.querySelector(".bg-container");
+
+
+nextBtn.addEventListener("click", nextSlide);
+prevBtn.addEventListener("click", prevSlide);
+
+
+// SLIDER 
+let counter = 1;
+
+function nextSlide() {
+  counter++;
+  checkCounter(counter);
+  bgContainer.style.background = `url(/img/bg-${counter}.jpeg) center/cover no-repeat`;
+}
+
+function prevSlide() {
+  counter--;
+  checkCounter();
+  bgContainer.style.background = `url(/img/bg-${counter}.jpeg) center/cover no-repeat`;
+}
+
+function checkCounter() {
+  if (counter > 3) {
+    counter = 1;
+  }
+  else if (counter < 1) {
+    counter = 3;
+  }
+  return counter;
+}
+
+// NAVBAR
+var navbar = document.querySelector("#navbar");
+var sticky = navbar.offsetTop;
+
+window.onscroll = function(){
+  stickyHeader();
+}
+
+function stickyHeader(){
+  if (window.pageYOffset > sticky){
+    navbar.classList.add("sticky");
+  }
+  else{
+    navbar.classList.remove("sticky");
+  }
+}
